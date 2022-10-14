cdf36639539e13930477af49842fcb7439ec1a7a
Golovin <oleg@prosite46.ru>
2022-10-14 12:28:31 +0300

    141022 dropdawn start

---
+++ js/dropdown.js
@@ -0,0 +1,34 @@
+/* ===== Выбор местоположения ===== */
+const wrapper = document.querySelector(".wrapper"),
+    selectBtn = wrapper.querySelector(".select-btn"),
+    searchInp = wrapper.querySelector("input"),
+    options = wrapper.querySelector(".options");
+let countries = ["Москва", "Санкт Петербург", "Нижний Новгород", "Тула", "Орел", "Воронеж", "Варкута",
+    "Липецк", "Владимир", "Екатеринбург", "Вологда", "Волгоград", "Иваново", "Брянск", "Бийск"];
+function addCountry(selectedCountry) {
+    options.innerHTML = "";
+    countries.forEach(country => {
+        let isSelected = country == selectedCountry ? "selected" : "";
+        let li = `<li onclick="updateName(this)" class="${isSelected}">${country}</li>`;
+        options.insertAdjacentHTML("beforeend", li);
+    });
+}
+addCountry();
+function updateName(selectedLi) {
+    searchInp.value = "";
+    addCountry(selectedLi.innerText);
+    wrapper.classList.remove("active");
+    selectBtn.firstElementChild.innerText = selectedLi.innerText;
+}
+searchInp.addEventListener("keyup", () => {
+    let arr = [];
+    let searchWord = searchInp.value.toLowerCase();
+    arr = countries.filter(data => {
+        return data.toLowerCase().startsWith(searchWord);
+    }).map(data => {
+        let isSelected = data == selectBtn.firstElementChild.innerText ? "selected" : "";
+        return `<li onclick="updateName(this)" class="${isSelected}">${data}</li>`;
+    }).join("");
+    options.innerHTML = arr ? arr : `<p style="margin-top: 10px;">Oops! Country not found</p>`;
+});
+selectBtn.addEventListener("click", () => wrapper.classList.toggle("active"));
